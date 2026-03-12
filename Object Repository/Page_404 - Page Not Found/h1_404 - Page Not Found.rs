c9c4efb3948866f69bc83cf47a9150eff96ebb0e
paulvanstraaten <paulvanstraaten@10.0.0.34>
2026-03-12 18:41:27 +1000

    This is my first commit

---
+++ Object Repository/Page_404 - Page Not Found/h1_404 - Page Not Found.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_404 - Page Not Found</name>
+   <tag></tag>
+   <elementGuidId>db14fb5f-db05-4f1c-b45c-437b45cf953a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' hero__heading ')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>.hero__heading</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value></value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>8d63a9d1-fb51-48a2-b48d-a4a5ca86d613</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>hero__heading astro-bbe6dxrz</value>
+      <webElementGuid>08c254f8-e56e-4c63-b7d6-b74a1a1c7ef9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>404 - Page Not Found</value>
+      <webElementGuid>e9784fa4-0401-4f90-bb2d-fb7496a032af</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-f9a33ec7b0318fda0dcccbdf7fdb79e2</value>
+      <webElementGuid>c9791dcd-280f-4c2d-bf97-efdd4f20f4fb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' hero__heading ')]</value>
+      <webElementGuid>ac6eb839-ef95-47ef-a37a-ddcb7d9460b7</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' hero__heading ')]</value>
+      <webElementGuid>cb50191c-1290-41ab-a7fc-f0084a0e3e6a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = '404 - Page Not Found' or . = '404 - Page Not Found')]</value>
+      <webElementGuid>1b0bab6d-3415-49f8-829c-3d1becd4982e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Suite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fb559056-e994-4a38-ab7f-507ad9adf0b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>7df45214-ce02-4618-b422-a08dce513616</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestLogin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
